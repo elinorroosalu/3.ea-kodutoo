76a261509709893d0b6cb6a44130a23373a6b8ad
mkivikin <mkivikin@users.noreply.github.com>
2018-05-24 12:19:34 +0300

    Extension now plays a sound if the notification pops up

--- limiter.js
+++ limiter.js
@@ -3,6 +3,7 @@
 // found in the LICENSE file.
 'use strict';
 
+
 chrome.alarms.onAlarm.addListener(function() {
     chrome.storage.sync.get(['reason'], function(item) {
     let icon = 'stopwatchlarge.png';
@@ -13,6 +14,9 @@ chrome.alarms.onAlarm.addListener(function() {
       icon: icon,
     }
     let notification = new Notification(title, options);
+    let alarmSound = new Audio();
+    alarmSound.src = "sound.wav";
+    alarmSound.play();
     chrome.browserAction.setBadgeText({text: ''});
   });
 });
